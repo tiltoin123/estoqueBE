2c2da353e9f600845462ec75a815c48cdc6fdcfe
RafaelValerini <rafael.valerini@icarotech.com>
2023-08-15 23:58:11 -0300

    fix port image

--- src/models/Message.ts
+++ src/models/Message.ts
@@ -44,7 +44,7 @@ class Message extends Model<Message> {
   @Column(DataType.STRING)
   get mediaUrl(): string | null {
     if (this.getDataValue("mediaUrl")) {
-      return `${process.env.BACKEND_URL}:${process.env.PROXY_PORT
+      return `${process.env.BACKEND_URL}
         }/public/${this.getDataValue("mediaUrl")}`;
     }
     return null;
